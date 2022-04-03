fc83dc0c6093a63a1e0b5b5a56222353902b0b45
Max <maksnokian8@gmail.com>
2022-04-03 19:53:08 +0300

    Lesson-2

--- app/src/main/java/ru/mydikan/notesappmvvm/navigation/NotesNavHost.kt
+++ app/src/main/java/ru/mydikan/notesappmvvm/navigation/NotesNavHost.kt
@@ -4,10 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import ru.mydikan.notesappmvvm.screens.Add
-import ru.mydikan.notesappmvvm.screens.Main
-import ru.mydikan.notesappmvvm.screens.Note
-import ru.mydikan.notesappmvvm.screens.Start
+import ru.mydikan.notesappmvvm.screens.*
 
 sealed class NavRoute(val route: String) {
     object Start: NavRoute("start_screen")
@@ -22,9 +19,9 @@ fun NotesNavHost() {
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = NavRoute.Start.route) {
-        composable(NavRoute.Start.route) { Start(navController = navController) }
-        composable(NavRoute.Main.route) { Main(navController = navController) }
-        composable(NavRoute.Add.route) { Add(navController = navController) }
-        composable(NavRoute.Note.route) { Note(navController = navController) }
+        composable(NavRoute.Start.route) { StartScreen(navController = navController) }
+        composable(NavRoute.Main.route) { MainScreen(navController = navController) }
+        composable(NavRoute.Add.route) { AddScreen(navController = navController) }
+        composable(NavRoute.Note.route) { NoteScreen(navController = navController) }
     }
 }
