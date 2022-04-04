5896ba33c7273871a2e2122259788a415b1dc87d
Max <maksnokian8@gmail.com>
2022-04-04 12:34:04 +0300

    Lesson-2

--- app/src/main/java/ru/mydikan/notesappmvvm/navigation/NotesNavHost.kt
+++ app/src/main/java/ru/mydikan/notesappmvvm/navigation/NotesNavHost.kt
@@ -18,9 +18,9 @@ fun NotesNavHost() {
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = NavRoute.Start.route) {
-        composable(NavRoute.Start.route) { Start(navController = navController) }
-        composable(NavRoute.Main.route) { Main(navController = navController) }
-        composable(NavRoute.Add.route) { Add(navController = navController) }
-        composable(NavRoute.Note.route) { Note(navController = navController) }
+        composable(NavRoute.Start.route) { StartScreen(navController = navController) }
+        composable(NavRoute.Main.route) { MainScreen(navController = navController) }
+        composable(NavRoute.Add.route) { AddScreen(navController = navController) }
+        composable(NavRoute.Note.route) { NoteScreen(navController = navController) }
     }
 }
